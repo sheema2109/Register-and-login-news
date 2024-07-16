13503ec670d36f13a663d740010d02c741b8b4a1
sheema2109 <sdsheema2005@gmail.com>
2024-07-16 22:16:30 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,213 @@
+<?php
+session_start();
+include("connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+  <a href="logout.php">Logout</a>
+    </div>
+</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>newspaper</title>
+  <style>
+ html,body{
+    padding:10px;
+    background-color:#ffefd5;
+}
+.container{
+  font-family:cambria;
+  font-size:20px;
+  }
+.container h1{
+  font-family: 'Sergia', serif;
+}
+.border{
+border-style:solid;
+}
+h1{
+  text-transform:uppercase;
+  font-size:30px;
+font-family:serif;
+}
+aside{
+width:150px;
+padding-left:15px;
+margin-right:15px;
+float:left;
+border-style:solid;
+padding-top:5px;
+}
+aside > p{
+ margin:0.5rem;
+}
+.newspaper{
+ column-count:2;
+ break-inside: avoid-column;
+}
+.newspaper1{
+  column-count:2;
+  break-inside: avoid-column;
+}
+h1{
+ text-size:300%;
+text-align:center;
+}
+h3{
+text-align:center;
+text-size:350%;
+}
+h2{
+text-align:center;
+text-size:100%;
+}
+.aside1 {
+  float:right;
+border-style:solid;
+}
+.aside2 {
+  float:right;
+border-style:solid;
+clear: right;
+}
+.image-with-description {
+  margin-bottom: 20px;  
+}
+.image-with-description img {
+  display: block;  
+}
+.image-with-description p {
+  margin: 0;  
+}
+footer {
+      padding: 3px;
+      text-align: center;
+      display: flex;
+    }
+.square-box1 {
+      width: 750px;
+      height: 350px;
+      border-width: 3px; 
+  border-style: double;
+  border-color: #000;  
+  margin-right: 10px ;
+    }
+    .square-box {
+      width: 450px;
+      height: 350px;
+      border-style: double;
+  border-color: #000;
+      margin-right: 10px; 
+    }
+ </style>
+</head>
+<body>
+<div class="container">
+  <h1>Saffron Sun</h1>
+</div>
+<div class="border">
+<p class="solid">
+<div class="subhead">MONDAY,MAY 13,2024</div> 
+</div>
+<img src="file:///C:/Users/SD.Rahmaasultana/Downloads/image.webp" width="120" height="90">
+<h4>This week</h4>
+<aside class="aside">
+<h6>EDITORIAL NEWS</h6>
+<h6>TOP HEADLINE</h6>
+<h6>CLASSFIELDS</h6>
+<h6>SPORTS</h6>
+<h6>WEATHER</h6>
+<p>43°Hi<br>RealFeel® 48°<br>RealFeel Shade™ 46°<br>Mostly sunny and very warm<br>Max UV Index-12 Extreme<br>Wind-SSE 13 km/h<br>Wind Gusts- 41 km/h
+  <br>Probability of Thunderstorms-0%<br>Precipitation-0.0 mm<br>Cloud Cover-17%</p>
+</aside>
+<aside class="aside1">
+  <h3>Sibling rivalry to the fore at YS family bastion Pulivendula</h3>
+  <p>Andhra Pradesh CM Y.S. Jagan Mohan Reddy is counting on four decades of family support from constituents, but must grapple with the challenge of his sister, now in the Congress, and a TDP candidate who has been preparing for the last year</p>
+  </aside>
+  <aside class="aside2">
+    <h3>India squad for T20 World Cup: Sanju Samson pips KL Rahul, Hardik Pandya named vice-captain</h3>
+    <p>India have announced their 15-man squad for the T20 World Cup in the West Indies and United States of America starting in June.</p>
+       </aside>
+<div class="para1">
+<article>
+<h1><b> LATEST HEADLINES</b></h1>
+<h2><b>READ ALL ABOUT IT</b></h2>
+<div class="newspaper">
+  <ul> <li>PM Narendra Modi to file nomination papers from Varanasi LS seat on May 14</li>
+  <li>Gudivada Amarnath releases his ‘Local Manifesto’ for Gajuwaka</li>
+  <li>The Lives of the Party: A Whirlwind History of the Met Gala</li>
+  <li>Benjamin Netanyahu’s Cabinet votes to permanently close Al Jazeera offices in Israel</li>
+  <li> Sunrisers Hyderabad eye consistency in must-win IPL clash against Mumbai Indians</li> 
+  <li>Indian awaiting details of arrests in Nijjar case in Canada,says jaishankar</li>
+<li>Bianca Bustamante Has Been Racing Against Boys Since She Was Six. Now She’s 19—And Out to Make History in Formula 1</li>
+<li>India adopts stringent norms for maximum pesticide residues limit in food items: Government</li>
+<li>Differently-abled passengers to get quota in all trains irrespective of concession </li>
+<li>Rahul Gandhi vows to replicate Telangana model of ‘pro-poor governance’ across the country</li>  </ul> 
+</div>
+</article>
+<hr>
+<article>
+<section>
+<h1>Rahul Gandhi vows to replicate Telangana model of ‘pro-poor governance’ across the country</h1>
+<div class="newspaper1">
+  The British Indian leader admitted his disappointment after Andy Street, the party’s popular Mayor of West Midlands, narrowly lost to Labour rival Richard Parker by a mere 1,508 votes.British Prime Minister Rishi Sunak on May 5 promised to work "as hard as ever" after a disastrous set of election results saw his Conservative Party being unseated across several local authorities and even losing a mayoralty stronghold in the West Midlands region of England.The British Indian leader admitted his disappointment after Andy Street, the party’s popular Mayor of West Midlands, narrowly lost to Labour rival Richard Parker by a mere 1,508 votes after a recount had to be ordered for the knife-edge result. This leaves Mr. Sunak with a solo sliver of hope in Ben Houchen, who had held on to the party’s mayoralty in Tees Valley on Friday amid a virtual sweep for the Opposition parties, with Labour in the lead.
+  <br>Congress leader Rahul Gandhi has asserted that the party will replicate the Telangana model of people’s government (guarantee schemes) with all-inclusive welfare and all-around development approach to empower the poor and marginalised sections including Adivasis across the country after coming to power at the Centre.
+ The Congress party is steadfast in its commitment to raise the 50% cap on reservations for SCs, STs and OBCs, he said while addressing an election meeting in Nirmal town of the poll-bound ST-reserved Adilabad Lok Sabha constituency in Telangana on Sunday afternoon.
+</div>
+</section>
+</div>
+<hr>
+<article>
+  <section>
+  <h1>Xi Visits Europe, Seeking Strategic Opportunity</h1>
+  <div class="newspaper1">
+    On his first visit to Europe in five years, the Chinese president, Xi Jinping, appears intent on seizing opportunities to loosen the continent’s bonds with the United States and forge a world freed of American dominance.
+The Chinese leader has chosen three countries to visit — France, Serbia and Hungary — that each, to a greater or lesser degree, look askance at America’s postwar ordering of the world, see China as a necessary counterweight and are eager to bolster economic ties.
+At a time of tensions with much of Europe — over China’s “no limits” embrace of Russia despite the war in Ukraine, its surveillance state and its apparent espionage activities that led to the recent arrest in Germany of four people — Mr. Xi, who is arriving in France on Sunday, wants to demonstrate China’s growing influence on the continent and pursue a pragmatic rapprochement.
+For Europe, the visit will test its delicate balancing act between China and the United States, and will no doubt be seen in Washington as a none-too-subtle effort by Mr. Xi to divide Western allies.He has timed his arrival at his second stop, Serbia, to coincide with the 25th anniversary of the deadly NATO bombing of the Chinese Embassy in Belgrade during the Kosovo war. That mistaken strike on May 7, 1999, for which the White House apologized, killed three Chinese journalists and ignited furious protests around the U.S. Embassy in Beijing.
+“For Xi, being in Belgrade is a very economical way to ask if the United States is really serious about international law,” said Janka Oertel, the director of the Asia program at the European Council on Foreign Relations in Berlin, “and to say, how about NATO overreach as a problem for other countries?”
+The Chinese government has continued to commemorate the Belgrade bombing, using it as an occasion to denounce what it sees as Western hypocrisy and bullying.
+</div>
+</section>
+</div>
+<hr>
+ <div class="newspaper">
+    <div class="image-with-description">
+    <h1>Bengal Governor CV Ananda Bose asks Raj Bhavan staff to ignore communication from Kolkata Police regarding molestation complaint</h1>
+    <img src="https://images.indianexpress.com/2024/05/ananda-bose-2.jpg?w=640" alt="Image Description" style="width: 350px; float: left; margin-right: 10px;">
+    <p>The move came a day after the Kolkata Police formed a special enquiry team (SET) to investigate the sexual harassment allegations levelled against Bose by a contractual woman employee at the Raj Bhavan.Bengal Governor C V Ananda Bose on Sunday wrote to Raj Bhavan staff and asked them to ignore any communication from the police regarding molestation charges levelled against him. He also told the Raj Bhavan staff that no criminal proceedings can be instituted against a Governor during their term of office.
+The move came a day after the Kolkata Police formed a special enquiry team (SET) to investigate the sexual harassment allegations levelled against Bose by a contractual woman employee at the Raj Bhavan.
+      Sources in the Kolkata Police said that the special team led by Deputy Commissioner (Central) Indira Mukherjee has started making inquiries into the incident. The special team comprises eight senior police officers.
+The woman employee had on Friday lodged a written complaint with the Kolkata Police alleging molestation by the West Bengal Governor in the Raj Bhavan. The Governor had then described the allegations as “absurd drama”, and said that none would be able to deter him from his “determined efforts to expose corruption and curb violence”.
+In the Sunday letter to the Raj Bhavan staff, Governor Bose said, “The reports from the media indicate that the police propose to conduct an inquiry into the incident and that they would be examining the staff of the Raj Bhavan. It is also reported that the investigating team intends to collect the CCTV footage from Raj Bhavan. The question that arises is as to whether the police can conduct an inquiry and collect evidence in view of the immunity enjoyed by the Governor under Art.361(2) and (3) of the Constitution of India.”</p>
+    </div>
+  </div>
+  <hr>
+<div class="newspaper">
+    <div class="image-with-description">
+      <h1>Hardik fan Nitish Reddy beginning to carve his own path</h1>
+      <img src="https://images.indianexpress.com/2024/04/Nitish.jpg" alt="Image Description" style="width: 350px; float: left; margin-right: 10px;">
+      <p>Most of his bowling success has come with the red ball, but the SRH all-rounder's T20 smarts with the bat are catching the eye.Fresh off his recall to the Indian team, Yuzvendra Chahal was guilty of conceding the most expensive over in Rajasthan Royals’ bowling innings. 21 runs came off the 13th over. IPL’s highest wicket taker found his match in 20-year-old Nitish Kumar Reddy. On Thursday night at the Uppal, the Sunrisers Hyderabad all-rounder was simply too good with the bat. Nitish’s 76* (42b, 3x4,8x6) was his second fifty in the season. And he’s had a couple of promising spells with his medium pace too.The bar is so low in Indian cricket.when any pace bowling all-rounder has a good day, the performance glows. Only the other day, Chief selector Ajit Agarkar made it clear that his committee saw no one who could challenge Hardik Pandya’s spot even when he’s endured a forgettable IPL season with bat and ball.
+         In Nitish’s case though, it’s no longer a case of one or two good days. With the ball, you may recall the slower bouncer that saw the back of Punjab Kings’ Jitesh Sharma. Those who follow Ranji trophy would remember his back-breaking spell for Andhra to get rid of Mumbai’s Shreyas Iyer with the short ball. Nitish has 52 first-class at an average of 23. And his T20 smarts with the bat are second to none. For someone so young, there’s certainly some promise.</p>
+    </div>
+  </div>
+  <hr>
+   <div class="newspaper">
+    <div class="image-with-description">
+      <h1>Elon Musk’s Tesla files trademark infringement case against Gurugram-based Tesla Power</h1>
+      <img src="https://images.indianexpress.com/2024/05/tesla-1200.jpg?w=640" alt="Image Description" style="width: 350px; float: left; margin-right: 10px;">
+      <p>Elon Musk’s electric vehicle (EV) maker Tesla Inc filed a trademark infringement case against Gurugram-based Tesla Power India Ltd amid discussions of Tesla’s entry into India, a Delhi High Court record showed on Friday.
+Tesla is incorporated in Delaware, United States and has accused the Indian company of using trade names “Tesla Power” and “Tesla Power USA”.
+         In its defence, Tesla Power told the court that the company has no intention to manufacture EVs “at all” and will not market other entities’ EVs as well under their trademark and tradename ‘Tesla Power USA’ or any other brand deceptively similar or use the word ‘Tesla’.The carmaker told the court that it discovered the Indian company was using its brand name in 2022 and tried to stop it from doing so, forcing it to file the lawsuit.
+“..a cease-and-desist notice was sent on 18th April 2022 to the defendant (Tesla Power)..since defendants continued to advertise and market their goods, the present suit was filed,” Tesla Inc told the court as per the record copy. The Delhi High Court judge allowed the Indian firm three weeks to submit written responses after it handed over a set of documents in support of its defence, the court record showed. The next hearing of the case has been set for May 22.
+        The case comes after Musk cancelled his much awaited visit to India on April 21 to meet Prime Minister Narendra Modi. A week later, Musk made a surprise visit to China to push for an advanced driver assistance package. Quoting a Tesla Power representative, Reuters reported that it has been present in India much before Musk’s Tesla and had all government approvals.</p>
+    </div>
+  </div>
+</body>
+</html>
+
+
